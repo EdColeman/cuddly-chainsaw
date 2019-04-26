54497a76548dca979b631260ef007438a44f3390
Ed Coleman <dev1@etcoleman.com>
2019-04-25 20:24:58 -0400

    checkpoint - wip

---
+++ event_prototype/core/src/main/java/org/apache/edcoleman/core/api/event/Event.java
@@ -0,0 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.edcoleman.core.api.event;
+
+import java.util.Map;
+
+public class Event {
+
+  private String traceId;      // required
+  private String type;         // required
+  private String id1;          // required
+  private String id2;          // optional
+  private long sequenceNumber; // optional
+  private long timestamp;      // required
+
+  private Map<String,String> annotations;  // optional
+
+}
