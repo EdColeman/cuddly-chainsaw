b10c50f7798a5940e0611679d2d58eb137196dc6
etcoleman <dev1@etcoleman.com>
2019-04-30 23:19:15 -0400

    checkpoint - wip
    added stub with sparkjava rest end point and dependecies
    added stub log4j2 dependecies

---
+++ fateMonitor/src/main/java/org/apache/edcoleman/cuddly_chainsaw/fateMonitor/reporter/X.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.edcoleman.cuddly_chainsaw.fateMonitor.reporter;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import static spark.Spark.*;
+
+public class X {
+
+    private static final Logger log = LogManager.getLogger();
+
+    public X(){
+        log.info("Echo...");
+        log.trace("Echo...");
+
+        get("/hello", (req, res) -> "Hello");
+
+
+        try {
+            Thread.sleep(20_000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ fateMonitor/src/test/java/org/apache/edcoleman/cuddly_chainsaw/fateMonitor/reporter/XTest.java
@@ -0,0 +1,16 @@
+package org.apache.edcoleman.cuddly_chainsaw.fateMonitor.reporter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class XTest {
+
+    @Test
+    public void echo(){
+        X x = new X();
+
+
+    }
+}
