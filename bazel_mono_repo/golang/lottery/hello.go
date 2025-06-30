692368b2f90adf25362c57fb0ccff93a5661808e
Ed Coleman <dev1@etcoleman.com>
2025-06-29 22:30:38 -0400

    adding sample functions

--- bazel_mono_repo/golang/lottery/hello.go
+++ bazel_mono_repo/golang/lottery/hello.go
@@ -4,4 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, Bazel! 💚")
+	anws := Bye()
+	fmt.Printf("said %d\n", anws)
+}
+
+func Bye() int {
+	fmt.Println("Good Bye, Bazel! 💚")
+	return 1
 }
