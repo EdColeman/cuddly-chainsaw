039cd2dd8d3edcad310a4234e52fa820d6b47d94
Ed Coleman <dev1@etcoleman.com>
2025-08-30 20:31:44 -0400

    add multiple file tests

--- eventMessage/python/main.py
+++ eventMessage/python/main.py
@@ -1,40 +1,71 @@
 import json
-from jsonschema import validate, ValidationError, Draft202012Validator
+import os
+from jsonschema import validate, ValidationError
 
 
 def main():
     print("Hello from python!")
 
-    schemaName = '../schema/event_message.schema.json'
-    inputName = '../schema/message1.json'
+    schema_name = '../schema/event_message.schema.json'
+    input_dir = '../schema/test'
 
-    # schema = {
-    #     "type": "object",
-    #     "properties": {
-    #         "name": {"type": "string"},
-    #         "age": {"type": "number"},
-    #     },
-    #     "required": ["name"],
-    # }
+    unexpected_error_count = 0
+    unexpected_error_files = []
+    missed_error_count = 0
+    missed_error_files = []
 
     try:
-        with open(schemaName, 'r') as f:
+        with open(schema_name, 'r') as f:
             schema = json.load(f)
 
-        with open(inputName, 'r', encoding='utf-8') as file:
-            message = json.load(file)
+        if not os.path.exists(input_dir):
+            print(f"No files found in directory {input_dir}")
+            exit(-1)
 
-        print(message)
+        files = os.listdir(input_dir)
 
-        # validate(instance={"name": "John", "age": 30}, schema=schema)
-        validate(instance=message, schema=schema)
+
+        print(f"files: {files}")
+
+        for filename in files:
+            input_name = os.path.join(input_dir, filename)
+
+            print(f"filename:{input_name}")
+
+            try:
+
+                with open(input_name, 'r', encoding='utf-8') as file:
+                    message = json.load(file)
+
+                    print(message)
+
+                    # validate(instance={"name": "John", "age": 30}, schema=schema)
+                    validate(instance=message, schema=schema)
+                    if "error" in filename:
+                        missed_error_count += 1
+                        missed_error_files.append(filename)
+
+            except FileNotFoundError:
+                print(f"Error: inputs json file not found: {input_name}")
+                exit()
+            except ValidationError:
+                if not "error" in filename:
+                    unexpected_error_files.append(filename)
+                    unexpected_error_count += 1
+            except json.JSONDecodeError:
+                unexpected_error_count += 1
+                print("Error: invalid schema, could not parse file")
+                exit()
 
     except FileNotFoundError:
         print("Error: schema file not found")
         exit()
-    except json.JSONDecodeError:
-        print("Error: invalid schema, could not parse file")
-        exit()
+
+    if unexpected_error_count > 0:
+        print(f"unexpected error count: {unexpected_error_count}, files: {unexpected_error_files}")
+
+    if missed_error_count > 0:
+        print(f"missed error count: {missed_error_count}, files: {missed_error_files}")
 
     #validate(instance={"name": "John", "age": "30"}, schema=schema)
 
